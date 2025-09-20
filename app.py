b89ec0310a9adfea366ddc3e1139d95b04535176
DerKaiser28 <chrisgrace281@gmail.com>
2025-09-21 00:55:32 +0530

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def root():
-    return "Hello from Cloud Run (CI/CD)!", 200
+    return "<h1>Hello from Cloud Run (CI/CD)! NOW UPDATED USING CI/CD</h1>", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
